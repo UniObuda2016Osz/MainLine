f2f876f16f92b4df6c1934d91a79e734687e676f
Rákóczi Botond <amandakanada@gmail.com>
2016-09-26 14:42:13 +0200

    App form added

--- Visual/src/com/app/visual/App.java
+++ Visual/src/com/app/visual/App.java
@@ -1,7 +1,22 @@
 package com.app.visual;
 
+import javax.swing.*;
+
 /**
  * Created by Rákóczi Botond on 2016. 09. 26..
+ * hibás package ????
  */
 public class App {
+    private JPanel MainPanel;
+    public App () {
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
