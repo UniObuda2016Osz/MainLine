6bba384e2e60bbed3239b6a2c4d563acf8e5498d
bogdanyijudit <judit.csonti@gmail.com>
2016-10-08 18:03:50 +0200

    Update VehicleDynamicsTest.java
    singleton test

--- DataBus/src/Dynamics/VehicleDynamicsTest.java
+++ DataBus/src/Dynamics/VehicleDynamicsTest.java
@@ -7,10 +7,19 @@ import static org.junit.Assert.assertEquals;
  * Created by BJudit on 2016. 10. 08..
  */
 public class VehicleDynamicsTest {
-    VehicleDynamics VD = new VehicleDynamics();
+    VehicleDynamics VD = VehicleDynamics.GetInstance();
 
     @org.junit.Before
     public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isVehicleDynamicsSingleton()
+    {
+        VehicleDynamics first = VehicleDynamics.GetInstance();
+        VehicleDynamics second = VehicleDynamics.GetInstance();
+
+        assertEquals(first == second, true);
     }
 
 
