19840c86b376ebfd90a12b8b73d5281f5f13442a
bogdanyijudit <judit.csonti@gmail.com>
2016-10-08 18:04:25 +0200

    Update Main.java
    Dynamics object

--- DataBus/src/Main.java
+++ DataBus/src/Main.java
@@ -1,11 +1,14 @@
+import Dynamics.VehicleDynamics;
+
 /**
  * Created by ral2bp on 2016.09.29..
  */
 public class Main {
+    private static VehicleDynamics vehicleDynamics;
     public static void main(String[] args){
         System.out.println("Main has started");
         /*Call modules in the logical order here*/
-
+        vehicleDynamics = VehicleDynamics.GetInstance();
 
     }
 
