23a60a38168cb5928e9b29bbc340f1ecf465e94c
Tamas Mitro <blacktom73@gmail.com>
2016-10-08 10:46:01 +0200

    Unit tesztek a Car osztály működéséhez

---
+++ DataBus/src/Visuals/CarTest.java
@@ -0,0 +1,54 @@
+package Visuals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Mitró Tamás on 2016.10.08..
+ */
+public class CarTest {
+
+    Car auto = new Car();
+
+    @Test
+    public void isInstanceOfAutoNotNull(){
+        Assert.assertNotNull(auto);
+    }
+
+    @Test
+    public void areXAndYCoordinateZero(){
+        Assert.assertEquals(0, auto.getXCoord());
+        Assert.assertEquals(0, auto.getYCoord());
+    }
+
+    @Test
+    public void isAutoNotCrashed(){
+        Assert.assertEquals(false, auto.isCrashed());
+    }
+
+    @Test
+    public void isAutoStarted(){
+        auto.startAuto(10);
+        Assert.assertEquals(true, auto.isMove());
+        Assert.assertEquals(10, auto.getSpeed());
+    }
+
+    @Test
+    public void isAccelerationTen(){
+        auto.accelerateAuto(10);
+        Assert.assertEquals(10, auto.getSpeed());
+    }
+
+    @Test
+    public void isAutoMoved(){
+        auto.accelerateAuto(10);
+        Assert.assertEquals(true, auto.isMove());
+    }
+
+    @Test
+    public void isAutoStopped(){
+        auto.stopAuto();
+        Assert.assertEquals(false, auto.isMove());
+        Assert.assertEquals(0, auto.getSpeed());
+    }
+}
