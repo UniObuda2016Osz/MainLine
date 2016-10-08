54e67d3c2c9c07df8f621fd60d67d20d69c1abdc
Gellért Bábel <gelli.babel94@gmail.com>
2016-10-08 12:44:18 +0200

    Vehicle Dynamics Test Class

---
+++ DataBus/src/Dynamics/VehicleDynamicsTest.java
@@ -0,0 +1,23 @@
+package Dynamics;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by BJudit on 2016. 10. 08..
+ */
+public class VehicleDynamicsTest {
+    VehicleDynamics VD = new VehicleDynamics();
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+
+
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        /*GCC will remove bus*/
+    }
+}
