5bb1a66452a33a67f0bf00f3bb712148ac1df8d9
szabobelazoltan <szabobelazoltan@gmail.com>
2016-10-12 21:54:40 +0200

    Demo is finished

--- DataBus/src/Dynamics/VehicleDynamicsDemoTimerListener.java
+++ DataBus/src/Dynamics/VehicleDynamicsDemoTimerListener.java
@@ -1,5 +1,6 @@
 package Dynamics;
 
+import Bus.Bus;
 /**
  * Created by Beci on 2016.10.12..
  */
@@ -12,9 +13,16 @@ public class VehicleDynamicsDemoTimerListener implements TimerListener {
     }
 
     private VehicleDynamics vehicledynamics = VehicleDynamics.GetInstance();
+    private Bus bus = Bus.getInstance();
 
     @Override
     public void OnTimerTick() {
-
+        vehicledynamics.Update();
+        System.out.print("Gas: " + bus.getGasPedal());
+        System.out.print("Brake: " + bus.getBrakePedal());
+        System.out.print("Gear: " + bus.getGearPosition());
+        System.out.print("Acceleration: " + bus.getAcceleration());
+        System.out.print("Speed: " + bus.getCurrentSISpeed());
+        System.out.print("\n");
     }
 }
