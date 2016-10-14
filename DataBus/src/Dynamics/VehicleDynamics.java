6a07747079773e545273426948a59060f0720d90
szabobelazoltan <szabobelazoltan@gmail.com>
2016-10-14 10:19:40 +0200

    Fixies added to VehicleDynamics

--- DataBus/src/Dynamics/VehicleDynamics.java
+++ DataBus/src/Dynamics/VehicleDynamics.java
@@ -10,8 +10,6 @@ import Dynamics.EnergyLoss;
  */
 public class VehicleDynamics {
     Bus bus;
-    Engine engine;
-    EnergyLoss energyLoss;
     private static VehicleDynamics instance;
 
     public static VehicleDynamics GetInstance(){
@@ -25,63 +23,118 @@ public class VehicleDynamics {
 
     }
 
-    public void SetSpeed()
-    {
-        float NewSpeed =  bus.getCurrentSISpeed() + engine.getAcceleration() ;
-        bus.setCurrentSISpeed(NewSpeed);
-    }
+    private final float GEAR_D_ACCELERATION_COEFFICIENT = 0.8f;
+    private final float GEAR_R_ACCELERATION_COEFFICIENT = 1.5f;
+    private final float GEAR_P_ACCELERATION_COEFFICIENT = 1.2f;
+    private final float GEAR_N_ACCELERATION_COEFFICIENT = 0f;
 
-    public void AccelerationDecrese() {
-        int NewAcceleration = engine.getAcceleration() - energyLoss.LossFromGround() - energyLoss.LossFromWeight() - energyLoss.LossFromWind() - energyLoss.LossFromCollision();
-        bus.setAcceleration( NewAcceleration );
-    }
+    private final float GEAR_D_MAX_SPEED = 50f;
+    private final float GEAR_R_MAX_SPEED = 8f;
+    private final float GEAR_P_MAX_SPEED = 4f;
+    private final float GEAR_N_MAX_SPEED = 0f;
 
-    public void AccelerationIncrease() {
-        int NewAcceleration = engine.getAcceleration() + bus.getGasPedal()/100 * engine.getHorsePower();
-        bus.setAcceleration( NewAcceleration );
-    }
+    // This is 7 meters per second speed reduction in a second at full brake
+    private final float BRAKE_PEDAL_DECELERATION_COEFFICIENT = 7f;
 
+    // This is the decleration of the car body, when the car is not driven and the brake pedal is not pressed
+    private final float STATIC_DECELERATION_COEFFICIENT = 0.2f;
 
-    public final int DRIVE_ENGINE_BREAK_DECLERATION = 2;
-    public final int PARK_ENGINE_BREAK_DECLERATION = 1;
-    public final int REVERSE_ENGINE_BREAK_DECLERATION = 1;
-    public final int NEUTRAL_ENGINE_BREAK_DECLERATION = 0;
-    public int getEngineBrakeDecleration() {
+    private float getGearAcceleration() {
         Bus.GearPosition gear = bus.getGearPosition();
-
-        if (gear == Bus.GearPosition.DRIVE) {
-            return DRIVE_ENGINE_BREAK_DECLERATION;
-        }
-
-        if (gear == Bus.GearPosition.PARK) {
-            return PARK_ENGINE_BREAK_DECLERATION;
+        if (gear == Bus.GearPosition.NEUTRAL) {
+            return GEAR_N_ACCELERATION_COEFFICIENT;
         }
 
         if (gear == Bus.GearPosition.REVERSE) {
-            return REVERSE_ENGINE_BREAK_DECLERATION;
+            return GEAR_R_ACCELERATION_COEFFICIENT;
         }
 
-        if (gear == Bus.GearPosition.NEUTRAL) {
-            return NEUTRAL_ENGINE_BREAK_DECLERATION;
+        if (gear == Bus.GearPosition.PARK) {
+            return GEAR_P_ACCELERATION_COEFFICIENT;
+        }
+
+        if (gear == Bus.GearPosition.DRIVE) {
+            return GEAR_D_ACCELERATION_COEFFICIENT;
         }
 
         return 0;
     }
 
-    private final int BRAKE_PEDAL_MAXIMUM_DECLERATION = 15;
-    private void declerationByBrakePedal() {
-        int pedal = bus.getBrakePedal();
-        int brakingdecleration = BRAKE_PEDAL_MAXIMUM_DECLERATION * pedal / 100;
 
-        int currentacceleration = bus.getAcceleration();
-        int accelerationdecreasedbybraking = currentacceleration - brakingdecleration;
-        bus.setAcceleration(accelerationdecreasedbybraking);
+    private float calculateGaspedalAcceleration() {
+        return bus.getGasPedal() / 100f;
     }
 
-    public void Update() {
-        Setspeed();
-        AccelerationDecrese();
-        AccelerationIncrease();
-        declerationByBrakePedal();
+
+    private float calculateBrakepedalDeceleration() {
+        return bus.getBrakePedal() / 100f * BRAKE_PEDAL_DECELERATION_COEFFICIENT;
+    }
+
+
+    private float calculateEngineBrakeDeceleration() {
+        float gearacceleration = this.getGearAcceleration();
+        return gearacceleration / 4;
+    }
+
+
+    private float calculateAcceleration() {
+        float gearacceleration = this.getGearAcceleration();
+        float gaspedalacceleration = this.calculateGaspedalAcceleration();
+        float enginebrakedecleration = this.calculateEngineBrakeDeceleration();
+        float brakepedaldeceleration = this.calculateBrakepedalDeceleration();
+        float staticdecleration = STATIC_DECELERATION_COEFFICIENT;
+
+        float gasacceleration = gaspedalacceleration * gearacceleration;
+        float decelerationsum = enginebrakedecleration + brakepedaldeceleration + staticdecleration;
+
+        float accelerationsum = gasacceleration - decelerationsum;// - this.acceleration;
+        return accelerationsum;
+    }
+
+
+    private float getGearMaxSpeed() {
+        Bus.GearPosition gear = bus.getGearPosition();
+        if (gear == Bus.GearPosition.NEUTRAL) {
+            return GEAR_N_MAX_SPEED;
+        }
+
+        if (gear == Bus.GearPosition.REVERSE) {
+            return GEAR_R_MAX_SPEED;
+        }
+
+        if (gear == Bus.GearPosition.PARK) {
+            return GEAR_P_MAX_SPEED;
+        }
+
+        if (gear == Bus.GearPosition.DRIVE) {
+            return GEAR_D_MAX_SPEED;
+        }
+
+        return 0;
+    }
+
+
+    private void updateAcceleration() {
+        float newacceleration = this.calculateAcceleration();
+        bus.setAcceleration((int)newacceleration);
+    }
+
+
+    private void updateSpeed() {
+        float gearmaxspeed = this.getGearMaxSpeed();
+        float currentspeed = bus.getCurrentSISpeed();
+        float currentacc = bus.getAcceleration();
+        float newspeed = Math.max(0, Math.min(currentspeed + currentacc, gearmaxspeed));
+
+        if (bus.getGearPosition() == Bus.GearPosition.REVERSE) {
+            newspeed = -newspeed;
+        }
+
+        bus.setCurrentSISpeed(newspeed);
+    }
+
+    public void update() {
+        this.updateAcceleration();
+        this.updateSpeed();
     }
 }
