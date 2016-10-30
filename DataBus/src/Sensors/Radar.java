8bb3ce2b68d3beacde2fe392160229956a2bd056
Roland Csegény <csegeny92@gmail.com>
2016-10-30 18:39:54 +0100

    Radar osztály elkezdve

--- DataBus/src/Sensors/Radar.java
+++ DataBus/src/Sensors/Radar.java
@@ -1,7 +1,32 @@
 package Sensors;
 
+import Environment.WorldObject;
+import Environment.XMLParserMain;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+
+import java.util.ArrayList;
+
 /**
  * Created by ral2bp on 2016.09.29..
  */
+
 public class Radar {
+
+
+    private ArrayList<WorldObject> detectedObjects;
+    private static final int radarDistance = 200;
+    private static final int radarRadiusInDegree = 30;
+    private double RadarXcoord;
+    private double RadarYcoord;
+    public Radar(){
+        detectedObjects = new ArrayList<>();
+
+
+    }
+
+    private void GetObjectsFromEnvironment(){
+        // this.detectedObjects =
+    }
+
+
 }
