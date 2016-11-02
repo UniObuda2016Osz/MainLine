bf94acae4787becf25b324c2d865029c122c759d
Akos <labancz.akos@gmail.com>
2016-11-02 17:26:30 +0100

    Három féle szenzorral kiegészítve és a forráskép elérési cím megadása is bekerült az NpcCar-ban. Módosítva lett a biciklis és a gyalogos leszármazása.

--- DataBus/src/Environment/NPC/Cyclist.java
+++ DataBus/src/Environment/NPC/Cyclist.java
@@ -5,8 +5,8 @@ import Environment.WorldObject;
 /**
  * Created by forii on 2016. 10. 14..
  */
-public class Cyclist extends WorldObject {
-    public Cyclist(int Id, int[] startPosition, int width, int height, double[] Transform, int Zlevel, int Opacity, boolean CanStuckOnIt) {
-        super(Id, startPosition, width, height, Transform, Zlevel, Opacity, CanStuckOnIt);
+public class Cyclist extends NPC {
+    public Cyclist(int Id, int[] startPosition, int Width, int Height, double[] Transform, int Zlevel, int Opacity, int movingDegree, int movingSpeed) {
+        super(Id, startPosition, Width, Height, Transform, Zlevel, Opacity, movingDegree, movingSpeed);
     }
 }
--- DataBus/src/Environment/NPC/Pedestrian.java
+++ DataBus/src/Environment/NPC/Pedestrian.java
@@ -5,8 +5,8 @@ import Environment.WorldObject;
 /**
  * Created by forii on 2016. 10. 14..
  */
-public class Pedestrian extends WorldObject {
-    public Pedestrian(int Id, int[] startPosition, int width, int height, double[] Transform, int Zlevel, int Opacity, boolean CanStuckOnIt) {
-        super(Id, startPosition, width, height, Transform, Zlevel, Opacity, CanStuckOnIt);
+public class Pedestrian extends NPC {
+    public Pedestrian(int Id, int[] startPosition, int Width, int Height, double[] Transform, int Zlevel, int Opacity, int movingDegree, int movingSpeed) {
+        super(Id, startPosition, Width, Height, Transform, Zlevel, Opacity, movingDegree, movingSpeed);
     }
 }
