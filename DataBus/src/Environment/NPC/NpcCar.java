e0dea0b6d0d8deaa8f6608f17fe81f2c727f0252
Akos <labancz.akos@gmail.com>
2016-11-02 21:59:24 +0100

    UserCar osztály létrehozva, NpcCar-ból helyezve a szenzorok.

--- DataBus/src/Environment/NPC/NpcCar.java
+++ DataBus/src/Environment/NPC/NpcCar.java
@@ -1,22 +1,9 @@
 package Environment.NPC;
 
-import Sensors.Camera;
-import Sensors.Radar;
-
 /**
  * Created by forii on 2016. 10. 14..
  */
 public class NpcCar extends NPC {
-
-    private String imagePath;
-    private Camera camera;
-    private Radar radar;
-    //private Ultrasonic ultrasonic;
-
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
 
     public NpcCar(int Id, int[] startPosition, int Width, int Height, double[] Transform, int Zlevel, int Opacity, int movingDegree, int movingSpeed) {
         super(Id, startPosition, Width, Height, Transform, Zlevel, Opacity, movingDegree, movingSpeed);
