a2c556551afd59295e7bb25efc5ffceeb2c4f1e5
Tamas Mitro <blacktom73@gmail.com>
2016-11-24 22:21:38 +0100

    Skeleton of ACCMain class lvl2

--- src/main/java/hu/oe/nik/autonomouscar/Functions/ACCMain.java
+++ src/main/java/hu/oe/nik/autonomouscar/Functions/ACCMain.java
@@ -1,18 +1,25 @@
 package hu.oe.nik.autonomouscar.Functions;
 
 /**
- * Created by ral2bp on 2016.09.29..
+ * Created by ral2bp on 2016.09.29. and by Mitró Tamás on 2016.11.24.
  */
 public class ACCMain {
 
+    private static ACCMain instance = null;
+    public static ACCMain getInstance(){
+        if (instance == null){
+            instance = new ACCMain();
+        }
+        return instance;
+    }
     private double targetSpeed;
     private double timegap;
     private double acceleration;
     private boolean isAccOn;
 
-    public ACCMain(double actualSpeed){
-        this.timegap = 1.0;
-        setTargetSpeed(actualSpeed);
+    private ACCMain(){
+        this.targetSpeed = 0;
+        this.timegap = 1.4;
         this.acceleration = 0.0;
         this.isAccOn = false;
     }
@@ -22,14 +29,15 @@ public class ACCMain {
     }
 
     public void setTargetSpeed(double targetSpeed) {
-        if(targetSpeed >= 30.0 && targetSpeed <= 180.0){
-            this.targetSpeed = targetSpeed;
-        } else if (targetSpeed < 30.0) {
-            this.targetSpeed = 30.0;
-        } else {
-            this.targetSpeed = 180.0;
+        if(isAccOn){
+            if(targetSpeed >= 30.0 && targetSpeed <= 180.0){
+                this.targetSpeed = targetSpeed;
+            } else if (targetSpeed < 30.0) {
+                this.targetSpeed = 30.0;
+            } else {
+                this.targetSpeed = 180.0;
+            }
         }
-
     }
 
     public double getTimegap() {
@@ -39,12 +47,12 @@ public class ACCMain {
     /**
      * If the passed parameter is above 0, this method will increase the timegap up to 2.0 with 0.2 steps,
      * if the passed parameter is below 0, this method will decrease the timegap down to 1.0 with 0.2 steps.
-     * @param timegap
+     * @param givenTimegap
      */
-    public void setTimegap(double timegap) {
-        if(timegap > 0 && this.timegap < 2.0){
+    public void setTimegap(int givenTimegap) {
+        if(givenTimegap > 0 && this.timegap < 2.0){
             this.timegap += 0.2;
-        } else if(timegap < 0 && this.timegap > 1.0){
+        } else if(givenTimegap < 0 && givenTimegap > 1.0){
             this.timegap -= 0.2;
         }
     }
@@ -56,12 +64,14 @@ public class ACCMain {
     /**
      * If the passed parameter is above 0, this method will increase the acceleration up to 3.5 m/s2 with 1.0 steps,
      * if the passed parameter is below 0, this method will decrease the acceleration down to -3.5 m/s2 with 1.0 steps.
-     * @param acceleration
+     * @param actualAcceleration
      */
-    public void setAcceleration(double acceleration) {
-        if(acceleration > 0 && this.acceleration < 3.5){
+    public void setAcceleration(int actualAcceleration) {
+        if(actualAcceleration > 0 && this.acceleration < 3.5){
+            // when the car is accelerating
             this.acceleration += 1.0;
-        } else if (acceleration < 0 && this.acceleration > -3.5){
+        } else if (actualAcceleration < 0 && this.acceleration > -3.5){
+            // when the car is slowing down
             this.acceleration -= 1.0;
         }
     }
@@ -70,8 +80,16 @@ public class ACCMain {
         return isAccOn;
     }
 
-    public void setAccOn() {
-        this.isAccOn = true;
+    /**
+     * This method can be started the ACC function
+     * @param actualSpeedOfCar
+     */
+    // At this invoke the ACCMain exists already
+    public void setAccOn(double actualSpeedOfCar) {
+        if(!isAccOn){
+            this.isAccOn = true;
+            this.targetSpeed = actualSpeedOfCar;
+        }
     }
 
     public void setAccOff() {
