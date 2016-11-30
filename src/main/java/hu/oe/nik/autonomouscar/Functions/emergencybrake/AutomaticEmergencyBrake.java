4b5908d0ff704431923ccefe05edec9fab367bfc
zsolt217 <zsoltfulop217@gmail.com>
2016-11-30 10:46:35 +0100

    AEB

--- src/main/java/hu/oe/nik/autonomouscar/Functions/emergencybrake/AutomaticEmergencyBrake.java
+++ src/main/java/hu/oe/nik/autonomouscar/Functions/emergencybrake/AutomaticEmergencyBrake.java
@@ -3,6 +3,8 @@ package hu.oe.nik.autonomouscar.Functions.emergencybrake;
 import hu.oe.nik.autonomouscar.Bus.Bus;
 import hu.oe.nik.autonomouscar.Environment.UserCar;
 
+import java.sql.Time;
+
 /**This module should sit on a UserCar instance
  * Created by levair on 2016. 11. 28..
  */
@@ -39,6 +41,22 @@ public class AutomaticEmergencyBrake {
     }
 
     private void watchObjects() {
-        //TODO: get the camera and do action if needed
+        // not hit emergency braking car
+        {
+
+        }
+
+        if(ownerCar.getSpeed()<50)//does not hit: pedestrian,cyclist, crossing car
+        {}
+        else if(ownerCar.getSpeed()<100)//does not hit: cutting in car
+        {}
+
+
     }
+    //for calculate the deceleration: must be > 4.5 m/s2, but below 10 m/s2
+    Time last_time;
+    int last_speed;
+    private void Brake ()
+    {}
+
 }
