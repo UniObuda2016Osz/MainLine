d986f2693c1961e62631363c82980e27d9bcfad4
Roland Levai <rav34@live.com>
2016-11-30 21:02:01 +0100

    preparing AEB demo

---
+++ src/main/java/hu/oe/nik/autonomouscar/Functions/emergencybrake/AutomaticEmergencyBrakeDemo.java
@@ -0,0 +1,18 @@
+package hu.oe.nik.autonomouscar.Functions.emergencybrake;
+
+import hu.oe.nik.autonomouscar.Environment.UserCar;
+
+/**
+ * The purpose of this module is to demo the AEB function.
+ * Created by levair on 2016. 11. 30..
+ */
+public class AutomaticEmergencyBrakeDemo {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
