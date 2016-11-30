9f27e62eba2735499745e10adb319356b23803d3
zsolt217 <zsoltfulop217@gmail.com>
2016-11-30 13:42:49 +0100

    EAB

--- src/main/java/hu/oe/nik/autonomouscar/Functions/emergencybrake/AutomaticEmergencyBrake.java
+++ src/main/java/hu/oe/nik/autonomouscar/Functions/emergencybrake/AutomaticEmergencyBrake.java
@@ -2,11 +2,22 @@ package hu.oe.nik.autonomouscar.Functions.emergencybrake;
 
 import hu.oe.nik.autonomouscar.Bus.Bus;
 import hu.oe.nik.autonomouscar.Environment.UserCar;
+import hu.oe.nik.autonomouscar.Environment.WorldObject;
+import hu.oe.nik.autonomouscar.Sensors.Radar.DetectedObject;
+import hu.oe.nik.autonomouscar.Sensors.Radar.Radar;
 
-/**This module should sit on a UserCar instance
+import java.sql.Time;
+import java.util.ArrayList;
+
+/**
+ * This module should sit on a UserCar instance
  * Created by levair on 2016. 11. 28..
  */
 public class AutomaticEmergencyBrake {
+
+    //a main ugye folyamatosan meghívja ezt, nekünk tudnunk kell az előző sebességet az akkori időt, hogy a mostani állapothoz képest tudjunk lassulást számolni
+    //plussz a vezető gázadását le kéne tiltani vészfékezés esetén
+    //for calculate the deceleration: must be > 4.5 m/s2, but below 10 m/s2
 
     private UserCar ownerCar;
     private boolean turnedOn;
@@ -33,10 +44,9 @@ public class AutomaticEmergencyBrake {
         if (turnedOn) {
 
             if (turnedOn == true) {
-                 watchObjects();
-                if(mustBreake()){
-                    Bus.getInstance().setEmergencyBrake(8); //parameterben a lassulás mértéke?
-                }
+                //if(vészfékezünk) Brake()
+                //else
+                watchObjects();
             } else {
                 //do nothing
             }
@@ -45,40 +55,47 @@ public class AutomaticEmergencyBrake {
     }
 
     private void watchObjects() {
-        // not hit emergency braking car
-        {
+        ownerCar.getRadar().RadarMain();
+        ArrayList<DetectedObject> objectsbeforethecar = Bus.getInstance().getFourNearestFromRadar();
+        for (DetectedObject object: objectsbeforethecar) {
+            // not hit emergency braking car
+            //if(object is vészfékező autó)
+            {
+                //mustBrake(object)
+            }
 
+            if (ownerCar.getSpeed() < 50)//does not hit: pedestrian,cyclist, crossing car
+            {
+            } else if (ownerCar.getSpeed() < 100)//does not hit: cutting in car
+            {
+            }
         }
-
-        if(ownerCar.getSpeed()<50)//does not hit: pedestrian,cyclist, crossing car
-        {}
-        else if(ownerCar.getSpeed()<100)//does not hit: cutting in car
-        {}
 
 
     }
-    //for calculate the deceleration: must be > 4.5 m/s2, but below 10 m/s2
     Time last_time;
     int last_speed;
-    private void Brake ()
-    {}
+    private void Brake() {
+        //user gázadás elvétele
+        //lassulást kiolvasása a korábbi adatokból=>fékerő állítás
+    }
 
-    private boolean mustBrake(){
+    private boolean mustBrake(DetectedObject object) {
 
-        double realSpeed = convertToMeterPerSec(ownerCar.getSpeed() - detectedObject.getActualSpeed());
-        double timeUntilImpact = detectedObject.getActualDistance() / realSpeed; // sec = meter / (m/s)
+        double realSpeed = convertToMeterPerSec(ownerCar.getSpeed() - object.getActualSpeed());//sebességkülönbség
+        double timeUntilImpact = object.getActualDistance() / realSpeed; // sec = meter / (m/s) -ütközésig az idő
 
-        double timeToZeroSpeed = realSpeed / 8; // sec = (m/s) / (m/s^2)
+        double timeToZeroSpeed = realSpeed / 8; // sec = (m/s) / (m/s^2) //sebességkülönbség nullára csökkenéséhez az idő vészfékezés mellett
         // Deceleration must be > 4.5 m/s2, but below 10 m/s2
 
-        if(timeToZeroSpeed >= timeUntilImpact)
+        if (timeToZeroSpeed >= timeUntilImpact)//ha nagyobb a megálláshoz szükséges idő mint ahogy vészfékezés mellett meg tudnánk állni akkor vészfékezünk??? WTF?
             return true;
         else
             return false;
     }
 
-    private double convertToMeterPerSec( double kmPerHour){
-        return (kmPerHour*1000) / 3600;
+    private double convertToMeterPerSec(double kmPerHour) {
+        return (kmPerHour * 1000) / 3600;
     }
 
 }
